e3d7de6daba05d23ea37ec490db304c2c7854ab2
Juliana Velasques <julianabalta16@gmail.com>
2021-03-16 14:39:13 -0400

    feat: updating

--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,14 +1,17 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContexts';
 import Styles from '../Styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
   return (
     <div className={Styles.profileContainer}>
       <img src="https://github.com/JulianaVelasques.png" alt="" />
       <div>
         <strong>Juliana Velasques</strong>
         <p>
-          <img src="icons/level.svg" alt="Level" />
-          Level 1
+          <img src="icons/level.svg" alt="level" />
+          Level {level}
         </p>
       </div>
     </div>
